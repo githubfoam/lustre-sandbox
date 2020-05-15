540090b2bdc0610e1928a0e26560868736e90b8d
githubfoam <lequestionis@gmail.com>
2020-05-15 23:15:31 +0300

    Every time my fingers touch brain, I'm Superfly T.N.T., I'm the Guns of the Navarone!

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,10 @@
 dnf install git -y
 git clone https://github.com/ComplianceAsCode/demo.git
 git clone https://github.com/ComplianceAsCode/content.git
+ls -lai
 cd content
-install_vm.py ./install_vm.py --distro fedora --domain test-suite-fedora
+ls -lai
+# install_vm.py ./install_vm.py --distro fedora --domain test-suite-fedora
 
 # apt-get update
 # apt-get install -y python3-dev python3-pip
