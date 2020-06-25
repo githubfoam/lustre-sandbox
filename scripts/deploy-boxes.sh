4b385ff5fc219899c1b04defc12f2f1d7b29c4af
githubfoam <lequestionis@gmail.com>
2020-06-25 19:16:38 +0300

    сторон`ам

--- scripts/deploy-boxes.sh
+++ scripts/deploy-boxes.sh
@@ -9,8 +9,8 @@ echo "==========================================================================
 vagrant plugin install vagrant-libvirt #The vagrant-libvirt plugin is required when using KVM on Linux
 vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with different providers
 
-vagrant box add "centos/7" --provider=virtualbox
-vagrant mutate "centos/7" libvirt
+vagrant box add "centos/7" --provider=libvirt
+# vagrant mutate "centos/7" libvirt
 vagrant up --provider=libvirt
 vagrant ssh c1 -c "hostnamectl status"
 
